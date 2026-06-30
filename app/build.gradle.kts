0c1da96bcff80a2b18eb97625a4fb504a03b5d57
UncleAndy123 <andrewburkholder277@gmail.com>
2026-06-30 19:09:09 -0400

    Update targetSdk to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.purespeech.notificationshortcutlauncher"
         minSdk = 24
-        targetSdk = 31
+        targetSdk = 33
         versionCode = 1
         versionName = "0.1"
 
